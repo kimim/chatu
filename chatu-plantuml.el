01408f52db6ddafca04618b99dfd48a54725493c
Kimi Ma <kimi.im@outlook.com>
2025-03-30 21:30:07 +0800

    use plantuml bin if mode is executable

--- chatu-plantuml.el
+++ chatu-plantuml.el
@@ -39,18 +39,19 @@ KEYWORD-PLIST contains parameters from the chatu line."
          (input-path (chatu-common-with-extension input-path "puml"))
          (output-path (plist-get keyword-plist :output-path))
          (input-dir (file-name-directory input-path))
-         (page (plist-get keyword-plist :page))
-         (output-ext (plist-get keyword-plist :output-ext)))
-    (concat "java -Dplantuml.include.path=" input-dir " -jar "
-	    plantuml-jar-path
-	    " -charset utf-8 -p -t"
-	    output-ext
-	    " "
-	    (when page (concat "-pipeimageindex " page))
-	    " < "
-	    input-path
-	    " > "
-	    output-path)))
+         (page (plist-get keyword-plist :page)))
+    (concat
+     (cond ((eq plantuml-default-exec-mode 'jar)
+            (concat "java -Dplantuml.include.path=" input-dir " -jar "
+                    plantuml-jar-path))
+           ((eq plantuml-default-exec-mode 'executable)
+            "plantuml"))
+            " -charset utf-8 -p -tsvg "
+            (when page (concat "-pipeimageindex " page))
+            " < "
+            input-path
+            " > "
+            output-path)))
 
 (defun chatu-plantuml-open (keyword-plist)
   "Open .puml file.
